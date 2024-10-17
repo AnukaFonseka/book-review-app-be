b82eb83fd86ef6afcb4ff6f55acb439ce6e50577
sewwwandifernando <sewwandinirushika7@gmail.com>
2024-10-17 10:53:50 +0530

    create get user by Id

--- controller/users.controller.js
+++ controller/users.controller.js
@@ -73,8 +73,35 @@ async function loginUser(req, res) {
     }
 }
 
+//Get User By ID
+async function getUserById(req, res) {
+    try {
+        const id = req.params.id;
+        const result = await userService.getUserById(id);
+
+        if(result.error) {
+            return res.status(result.status).json ({
+                error: true,
+                payload: result.payload
+            })
+        } else {
+            return res.status(result.status).json ({
+                error: false,
+                payload: result.payload
+            })
+        }
+
+    } catch (error) {
+        return res.status(500).json ({
+            error: true,
+            payload: error
+        })
+    }
+}
+
 module.exports = {
     registerUser,
-    loginUser
+    loginUser,
+    getUserById
 }
 
