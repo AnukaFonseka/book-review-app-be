757127e3a2aecdef64e81c2ea9bd12cc38b88225
sewwwandifernando <sewwandinirushika7@gmail.com>
2024-10-19 23:40:15 +0530

    Add auth middleware

--- routes/books.routes.js
+++ routes/books.routes.js
@@ -1,15 +1,19 @@
 const express = require('express')
 const bookController = require('../controller/books.controller')
+const authMiddleware = require("../middlware/auth.middleware");
 
 function bookRoutes() {
     const router = express.Router()
 
     router.use(express.json());
 
-    router.post("/", bookController.addBook);
     router.get("/", bookController.getAllBooks);
-    router.put("/:id", bookController.updateBook);
     router.get("/:id", bookController.getBookById);
+
+    router.use(authMiddleware);
+    
+    router.post("/", bookController.addBook);
+    router.put("/:id", bookController.updateBook);
     router.delete("/:id", bookController.deleteBookById);
 
 
